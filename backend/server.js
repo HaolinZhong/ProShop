43c787fa346c8b288d143148c01ace61c7be6421
HaolinZhong <hz2771@columbia.edu>
2022-06-02 11:51:03 -0400

    add errorHandler middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import productRoute from "./route/productRoutes.js"
+import {notFound, errorHandler} from "./middleware/errorMiddleware.js"
 
 dotenv.config();
 
@@ -11,6 +12,9 @@ const app = express();
 
 app.use("/api/products", productRoute);
 
+app.use(notFound);
+app.use(errorHandler);
+
 app.get("/", (req, res) => {
     res.send("API is running");
 })
@@ -19,3 +23,4 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 
 app.listen(port, console.log(`Server started on port ${port} in ${process.env.NODE_ENV} mode!`));
+

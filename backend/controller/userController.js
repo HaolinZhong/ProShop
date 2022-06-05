8fa4eaf918b761290b50f1cfec94998d65ad9303
HaolinZhong <hz2771@columbia.edu>
2022-06-05 13:10:31 -0400

    add user registration functionality in backend

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -4,7 +4,7 @@ import generateToken from "../utils/generateToken.js";
 
 // @desc    User authentification
 // @route   POST /api/users/login
-// @access  Private
+// @access  Public
 const authUser = expressAsyncHandler(async (req, res) => {
     const { email, password } = req.body
 
@@ -23,6 +23,41 @@ const authUser = expressAsyncHandler(async (req, res) => {
         throw new Error('Invalid email or password')
     }
 })
+
+
+// @desc    User registration
+// @route   POST /api/users
+// @access  Public
+const registerUser = expressAsyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email: email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    } 
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
 
 
 // @desc    Get user profile 
@@ -44,4 +79,4 @@ const getUserProfile = expressAsyncHandler(async (req, res) => {
 })
 
 
-export { authUser, getUserProfile }
+export { authUser, getUserProfile, registerUser }

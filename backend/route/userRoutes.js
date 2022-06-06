90b2ef756a50a59a4653e9e3723f1efc1539990a
HaolinZhong <hz2771@columbia.edu>
2022-06-06 01:34:50 -0400

    add update user profile functionality in backend

--- backend/route/userRoutes.js
+++ backend/route/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authUser, getUserProfile, registerUser } from "../controller/userController.js"
+import { authUser, getUserProfile, registerUser, updateUserProfile } from "../controller/userController.js"
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -11,5 +11,6 @@ router.post("/login", authUser);
 
 router.route("/profile")
     .get(protect, getUserProfile)
+    .put(protect, updateUserProfile)
 
 export default router;
